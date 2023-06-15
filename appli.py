07b548b993e49f79dc99d36f541270af40401026
Laxiss-UwU <dimitrijasica60600@gmail.com>
2023-06-15 14:08:02 +0200

    updates

--- appli.py
+++ appli.py
@@ -11,38 +11,55 @@ mysql = MySQL(app)
 
 @app.route("/", methods = ["POST", "GET"])
 def login():
-
-	return render_template("index.html")
-
 	if request.method == "POST":
 		username = request.form["username"]
 		password = request.form["password"]
 		cursor = mysql.connection.cursor()
-		query = "SELECT * FROM users WHERE username = %s AND password = %s"
+		query = "SELECT username, password FROM users WHERE username = %s AND password = %s"
 		cursor.execute(query, (username, password))
-		result = cursor.fetchone()
+		result = cursor.fetchall()
 		cursor.close()
 
 		if result is not None: 
 			return render_template("login.html")
 		else:
-			return 'Identifiants incorrects. Veuillez réessayer.'
 			return render_template("index.html")
 
-@app.route('/register.html', methods=['GET', 'POST'])
-def register():
-	if request.method == 'POST':
+	return render_template("index.html")
+
+@app.route("/index.html", methods = ["POST", "GET"])
+def login():
+
+	if request.method == "POST":
 		username = request.form["username"]
 		password = request.form["password"]
 		cursor = mysql.connection.cursor()
-		query = "INSERT INTO users(username,password) VALUES(%s,%s)"
+		query = "SELECT username, password FROM users WHERE username = %s AND password = %s"
 		cursor.execute(query, (username, password))
-		result = cursor.fetchone()
+		result = cursor.fetchall()
 		cursor.close()
-		return 'Inscription réussie !'
 
-	return render_template('register.html')
+		if result is not None: 
+			return render_template("login.html")
+		else:
+			return render_template("index.html")
+
+	return render_template("index.html")
+
+@app.route("/register.html", methods=["GET", "POST"])
+def register():
+
+	if request.method == "POST":
+		username = request.form["id"]
+		password = request.form["mdp"]
+		cursor = mysql.connection.cursor()
+		query = "INSERT INTO users (username, password) VALUES (%s, %s)"
+		cursor.execute(query, (username, password))
+		mysql.connection.commit()
+		cursor.close()
+		return render_template("inscrit.html")
+	
+	return render_template("register.html")
 
 if __name__ == "__main__":
     app.run()
-
